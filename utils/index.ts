b63d1ef47af58e8ab36c128a4644c9a7458ef7b3
zhangzhengguang <zhangzhe7432@qq.com>
2023-02-20 18:32:47 +0800

    feat: eslint

--- utils/index.ts
+++ utils/index.ts
@@ -1,38 +1,56 @@
 /** 获取url字符串里面参数 */
-export function getUrlParams(url: string): {[propName: string]:  string} {
-      let params: {[propName: string]:  string} = {}; // 用于保存变量和值的Map对象
-      const searchParams = new URLSearchParams(url.slice(url.indexOf('?') + 1)) // 将URL的查询参数解析为URLSearchParams对象
-      // 遍历URLSearchParams对象，将变量和值存储到params中
-      searchParams.forEach((value, key) => {
-            params[key] = value
-      })
-      return params
+export function getUrlParams(url: string): { [propName: string]: string } {
+  const params: { [propName: string]: string } = {}; // 用于保存变量和值的Map对象
+  const searchParams = new URLSearchParams(url.slice(url.indexOf('?') + 1)) // 将URL的查询参数解析为URLSearchParams对象
+  // 遍历URLSearchParams对象，将变量和值存储到params中
+  searchParams.forEach((value, key) => {
+    params[key] = value
+  })
+  return params
 }
 
 
 /** 求字符串数组的最大公共前缀 */
 export function longestCommonPrefix(strs: string): string {
-      /*
+  /*
       实现思路：
             1, 将数组中的第一项作为最长公共前缀的值，
             2，依次遍历数组中的其他的字符串，将当前的最长公共前缀和当前值进行比较
       */
-      if(strs.length === 0 || !strs) {
-            return ''
+  if (strs.length === 0 || !strs) {
+    return ''
+  }
+  let prefix: string = strs[0]
+  for (let index = 1; index < strs.length; index++) {
+    // strs[index].indexOf(prefix) 只有等于零的时候，才说明当前最大公共前缀也是当前字符串的前缀
+    while (strs[index].indexOf(prefix) !== 0) {
+      // 如果最大公共前缀不在当前字符串的首部，去除最大公共前缀的末尾的值，继续比较
+      prefix = prefix.substring(0, prefix.length - 1)
+      // 最大公共前缀为‘’，说明没有最大公共前缀
+      if (prefix === '') {
+        return ''
       }
-      let prefix: string = strs[0]
-      for (let index = 1; index < strs.length; index++) {
-            // strs[index].indexOf(prefix) 只有等于零的时候，才说明当前最大公共前缀也是当前字符串的前缀
-            while(strs[index].indexOf(prefix) !== 0){
-                  // 如果最大公共前缀不在当前字符串的首部，去除最大公共前缀的末尾的值，继续比较
-                  prefix = prefix.substring(0, prefix.length - 1)
-                  // 最大公共前缀为‘’，说明没有最大公共前缀
-                  if(prefix === ''){
-                        return ''
-                  }
-            }
-      }
-          return prefix
+    }
+  }
+  return prefix
 }
 
 /** 使用递归实现深拷贝 */
+export function deepCopy<T>(obj: T): T {
+  if (typeof obj !== 'object' || obj === null) {
+    // 如果是基本类型或 null，则直接返回
+    return obj;
+  }
+    
+  if (Array.isArray(obj)) {
+    // 如果是数组，则遍历数组，递归调用 deepCopy
+    return obj.map(deepCopy) as any;
+  }
+    
+  // 否则是普通对象，需要遍历对象的所有属性，递归调用 deepCopy
+  const result = {} as T;
+  for (const key in obj) {
+    result[key] = deepCopy(obj[key]);
+  }
+  return result;
+}
